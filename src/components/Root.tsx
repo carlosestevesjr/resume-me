c7cb6da78a938e9f6adba31474cabdcb5d6e43c7
carlosestevesjr <carlosestevesjr0@gmail.com>
2022-11-14 00:16:35 -0300

    Normalizando

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,7 +1,10 @@
-
+import React from 'react'
+import { Header, Home, Footer, About, Education, Experience, Services, Review, Tools, Contact } from '../sections'
+import { Row, Col, Container } from 'react-bootstrap'
+import ScrollTopArrow from '../components/Scroller'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { GlobalStyle } from "../styles/globals"
 
 
 import '../components/Icons'
@@ -9,7 +12,28 @@ import '../components/Icons'
 function App() {
     return (
         <>
-            <div>eu</div>
+            <GlobalStyle />            
+            <Header />
+            <main>
+                <Home />
+                <About />
+                <Container>
+                    <Row>
+                        <Col md={6}>
+                            <Education />
+                        </Col>
+                        <Col md={6}>
+                            <Experience />
+                        </Col>
+                    </Row>
+                </Container>
+                <Services />
+                {/*<Review />
+                <Tools />
+                <Contact /> */}
+            </main>
+            <Footer />
+            <ScrollTopArrow />
         </>
     )
 }
